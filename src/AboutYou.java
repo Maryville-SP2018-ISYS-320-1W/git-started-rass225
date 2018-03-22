29e0e25b45a34329c11ca6bd1b667660840e042e
rass225 <31581061+rass225@users.noreply.github.com>
2018-03-22 02:34:34 +0200

    changes

--- src/AboutYou.java
+++ src/AboutYou.java
@@ -3,11 +3,12 @@ public class AboutYou {
 
 	public static void main(String[] args) {
 		// TODO Modify the values in the String literals in ALL_CAPS below
-		System.out.print("Hello, my name is ");
-		System.out.println("YOUR_NAME");
+		System.out.print("Hello, my name is RASMUS TAUTS ");
+		
+		System.out.println("RASMUS TAUTS");
 
-		System.out.print("And I'm taking this class because ");
-		System.out.println("BECAUSE_WHY");
+		System.out.print("And I'm taking this class BECAUSE I WANT TO LEARN JAVA PROGRAMMING LANGUAGE ");
+		System.out.println("I WANT TO GET TO KNOW JAVA LANGUAGE");
 	}
 
 }
